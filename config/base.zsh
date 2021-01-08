420fc11573f3f90f00a1e0b6d0a8ffdfda82bed2
@slovacus <slovacus@gmail.com>
2021-01-08 05:50:06 -0500

    chore: implement conf and config (#3)

---
+++ config/base.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export KITTY_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export KITTY_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
+export KITTY_MESSAGE_YAY="Please install yay or use antibody bundle luismayta/zsh-yay branch:develop"
+export KITTY_PACKAGE_NAME="kitty"
+
+export KITTY_APPLICATION="$(command -v kitty)"
+export KITTY_CONF_DIR=${HOME}/.config/kitty
+export KITTY_FILE_SETTINGS="${KITTY_CONF_DIR}"/kitty.conf
+export KITTY_THEMES_DIR=${KITTY_CONF_DIR}/themes
+[ -z "${EDITOR}" ] && export EDITOR="vim"
