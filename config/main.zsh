b514884da192cabcf0d8da5bf0516b5b5a9fa9b7
Luis Mayta <luis@hadenlabs.com>
2021-01-08 05:50:06 -0500

    chore: implement conf and config (#3)

---
+++ config/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function kitty::config::main::factory {
+    # shellcheck source=/dev/null
+    source ${ZSH_KITTY_PATH}/config/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source ${ZSH_KITTY_PATH}/config/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source ${ZSH_KITTY_PATH}/config/linux.zsh
+      ;;
+    esac
+}
+
+kitty::config::main::factory
